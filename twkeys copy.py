1a69eb2891394202da0f64fc25ecac49b3da9459
Isabel Yepes <iyepes0120@gmail.com>
2019-10-08 05:24:24 -0500

    Sentiment Analysis
    Código para el taller en SciPy Latam 2019

---
+++ twkeys copy.py
@@ -0,0 +1,16 @@
+#Credenciales del Twitter API
+def consumer_key():
+	#Add Consumer Key (API Key)
+	return "put your consumer key here"
+
+def consumer_secret():
+	#Add Consumer Secret (API Secret)
+	return "put your consumer secret here"
+
+def access_key():
+	#Add Access Token
+	return "put your access key here"
+
+def access_secret():
+	#Add Access Token Secret
+	return "put your access secret here"
