d54c8c1e43293b5a25eb8bb06eb79a91bf3ea5f3
Isabel Yepes <iyepes0120@gmail.com>
2019-10-08 11:05:33 -0500

    Add files via upload

--- twkeys.py
+++ twkeys.py
@@ -1,16 +1,16 @@
 #Credenciales del Twitter API
 def consumer_key():
 	#Add Consumer Key (API Key)
-	return "put your consumer key here"
+	return "bphRC8L9YwgArcTWl6QAxd8ZW"
 
 def consumer_secret():
 	#Add Consumer Secret (API Secret)
-	return "put your consumer secret here"
+	return "EJ0w5sJ48hRHeY5JsH5rZyTe0YzJz5gI0q3HIrFWjwqv6L4PgO"
 
 def access_key():
 	#Add Access Token
-	return "put your access key here"
+	return "217885674-FQJBc09Vfd86pqcYFaetsVK3cz6gNziJfpuQfe6j"
 
 def access_secret():
 	#Add Access Token Secret
-	return "put your access secret here"
+	return "gjPiyosXOGDbYSGuisUvAs50JmBduXF6L33YwXubPPjpE"
